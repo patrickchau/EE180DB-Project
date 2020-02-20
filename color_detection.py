ff5e2e688cbe182d20f2a9d9db9cc5c903b34072
patrickchau <patrickchau59@gmail.com>
2020-02-20 15:28:31 -0800

    added color detection code

---
+++ color_detection.py
@@ -0,0 +1,69 @@
+import numpy as np
+import argparse
+import cv2
+
+# values given in HSV space
+boundaries = [
+    ([86,0,0],[120,255,255]),    # blue
+    ([20,0,0],[35,255,255]),    # yellow
+    ([40,0,0],[80,255,255]),    # green
+    ([0,50,20],[15,255,255])     # red / human
+]
+
+# need to return the center and draw circles on HSV image
+
+# code from pyimagesearch
+def grab_contours(cnts):
+    if len(cnts) == 2:
+        cnts = cnts[0]
+    elif len(cnts) == 3:
+        cnts = cnts[1]
+    else:
+        raise Exception(("Contours tuple must have length 2 or 3, "
+            "otherwise OpenCV changed their cv2.findContours return "
+            "signature yet again. Refer to OpenCV's documentation "
+            "in that case"))
+    # return the actual contours array
+    return cnts
+
+# given a mask and the image, find the largest contour and the center of it
+def find_color_point( color, image ):
+    hsv = cv2.cvtColor(image.copy(), cv2.COLOR_BGR2HSV) # convert to HSV values
+    mask = get_color_mask(color, hsv.copy())            # get the bitmap of colors which match the boundary chosen
+    output = cv2.bitwise_and(hsv, hsv, mask=mask)       # apply mask to image
+
+    # find contours in the image
+    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    # function to ensure compatibility with any version of opencv
+    cnts = grab_contours(cnts)
+    center = None
+
+    # given the contours, find a minimum bounding circle
+    if len(cnts) > 0:
+        # if there are found contours, find the largest one
+        c = max(cnts, key=cv2.contourArea)
+        ((x,y), radius) = cv2.minEnclosingCircle(c)
+        M = cv2.moments(c)
+        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+
+	# only proceed if the radius meets a minimum size
+    if radius > 10:
+		# draws the circle onto the original image
+        cv2.circle(image, (int(x), int(y)), int(radius),
+            (0, 255, 255), 2)
+        cv2.circle(image, center, 5, (0, 0, 255), -1)
+    
+    # convert the masked image back to BGR for display
+    converted = cv2.cvtColor(output,cv2.COLOR_HSV2BGR)
+    return converted
+
+def get_color_mask(color, image):
+    # assuming that image is given in HSV
+    mask = None
+    if color >= 0 and color <= 3:
+        low = np.array(boundaries[color][0], dtype = "uint8")   # pull the correct color boundaries
+        up = np.array(boundaries[color][1], dtype = "uint8")
+        mask = cv2.inRange(image.copy(), low, up)             # mask the image according to boundary
+        mask = cv2.erode(mask, None, iterations = 2) 
+        mask = cv2.dilate(mask, None, iterations = 2) # removes small bumps outside of main
+    return mask # returns the mask and the masked image
--- facedetect.py
+++ facedetect.py
@@ -19,6 +19,7 @@ import re
 import sys
 import time
 import getopt
+import color_detection as cd
 
 # local modules
 from video import create_capture
@@ -34,7 +35,8 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 abs_path = os.path.dirname(__file__)                       # local path
 rel_path = 'img.png'
 
-def detect(img, cascade): # detects all the faces and assigns bounding boxes to them
+# detects all the faces and assigns bounding boxes to them
+def detect(img, cascade): 
     rects = cascade.detectMultiScale(img, scaleFactor=1.1, minNeighbors=4, minSize=(30, 30),
                                      flags=cv.CASCADE_SCALE_IMAGE)
     if len(rects) == 0:
@@ -42,11 +44,20 @@ def detect(img, cascade): # detects all the faces and assigns bounding boxes to 
     rects[:,2:] += rects[:,:2]
     return rects
 
+# given a set of coords, draw the rectangles onto the image
 def draw_rects(img, rects, color):
     for x1, y1, x2, y2 in rects:
         cv.rectangle(img, (x1, y1), (x2, y2), color, 2)
 
+def find_rects(img, cascade):
+    gray = cv.cvtColor(img.copy(), cv.COLOR_BGR2GRAY)
+    gray = cv.equalizeHist(gray)
+    rects = detect(gray, cascade) # the large bounding boxes around faces
+    print (rects)
+    return rects
+
 def main():
+    # first see if there is access to a webcam to use as source
     args, video_src = getopt.getopt(sys.argv[1:], '', ['cascade=', 'nested-cascade='])
     try:
         video_src = video_src[0]
@@ -59,6 +70,7 @@ def main():
     cascade_fn = args.get('--cascade', "haarcascades/haarcascade_frontalface_alt.xml")
     nested_fn  = args.get('--nested-cascade', "haarcascades/haarcascade_eye.xml")
 
+    # files provided from OpenCV sample file for face detection
     cascade = cv.CascadeClassifier(cv.samples.findFile(cascade_fn))
     nested = cv.CascadeClassifier(cv.samples.findFile(nested_fn))
 
@@ -66,119 +78,77 @@ def main():
     cam = create_capture(video_src, fallback='synth:bg={}:noise=0.05'.format(cv.samples.findFile('lena.jpg')))
 
     _ret, img = cam.read()
-    gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-    gray = cv.equalizeHist(gray)    
-    rects = detect(gray, cascade) # the large bounding boxes around faces
-    print("This is the inital reading: ")
-    print(rects)
+    # rects = detect(gray, cascade) # the large bounding boxes around faces
 
-    face_rects = rects.copy()
     # continuously check the camera and update the bounding boxes
     while True:
         # read from the camera and turn into grayscale
         _ret, img = cam.read()
-        gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
-        gray = cv.equalizeHist(gray)
-
-        t = clock()
-        rects = detect(gray, cascade) # the large bounding boxes around faces
-        print(rects)
-
-        # update face_rects based on new values
-        # problem is that rects occurs in a random order
-        # so associate position w/ least norm
-        death = rects.copy() # this is already a numpy array
-         
-        """
-        doesnt work lol
-        # if a new face appears, 
-        # itll be the furthest off from what's already there
-        # therefore for each entry in the updated faces, 
-        # see which has the least norm and exclude that one
-        choice = 0
-        c = 0
-        for cur in rects:
-            cur_np = np.array(cur)
-            count = 0
-            least = 10000
-            for comp in face_rects:
-                comp_np = np.array(comp)
-                if( np.linalg.norm(cur_np-comp_np) < least ): # new least norm
-                    choice = count
-                count += 1
-            death = np.delete(death, choice)
-            if c < len(face_rects):
-                face_rects[c] = rects[choice]
-            c+=1
-
-        # if there's an extra face, just append it
-        if (len(rects) == len(face_rects)+1):
-            #face_rects.append(death[0])
-            print("this is face")
-            print(face_rects)
-            print("this is death")
-            print(death)
-            face_rects = np.concatenate(face_rects, death[0])
+        # clock for measuring time in between frames
         
-        print(face_rects)
-        """
+        # find the bounding boxes for faces
+        rects = find_rects(img, cascade)
+
+        #gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+        #gray = cv.equalizeHist(gray)
+        t = clock() 
+        #rects = detect(gray, cascade) # the large bounding boxes around faces
 
         # draws the rectangles that make up the bounding box
+        # vis is the copy of the raw image that we will use for processing, so as to not mess up raw image
         vis = img.copy()
         draw_rects(vis, rects, (0, 255, 0))
-        if not nested.empty():
-            for x1, y1, x2, y2 in rects:
-                roi = gray[y1:y2, x1:x2]
-                vis_roi = vis[y1:y2, x1:x2]
-                subrects = detect(roi.copy(), nested)
-                draw_rects(vis_roi, subrects, (255, 0, 0))
-        dt = clock() - t
 
+        # find_color_point will return an image with all other colors except desired color masked out
+        # will also have the image of the circle on it
+        # see color_detection.py for source code
+        # out = cd.find_color_point(1, vis)
+        
+        # adds the timestamp in top left
+        dt = clock() - t
         draw_str(vis, (20, 20), 'time: %.1f ms' % (dt*1000))
+
+        # draw the image after all has been put onto it
         cv.imshow('facedetect', vis)
 
-        # currently set to only display the image of the person on the left hand side of screen
+        # goal here is to get the circle and the raw image 
+        # so we need to pass the center and the image on the raw image
+        # currently set to only save the image of the person on the left hand side of screen
         if (len(rects) > 0):    # if rects is not empty, update on unity end
-            x_marg = 0 
-            y_marg = 0
-            kop = 0
-            x1 = 0
-            x2 = 0
-            y1 = 0
-            y2 = 0
+            origin = (0,0) # the point from which distances are calc'd
+            (x_marg, y_marg) = (0,0)
+            (x1,x2,y1,y2) = (0,0,0,0)
             smallest = 25000
             for update in rects:
                 co = '' # x1 y1 x2 y2
-                flag = False
-                count = 0
-                for coord in update:
-                    co += str(coord) + ' '
-                    if not flag and update[0] < smallest:
-                        smallest = update[0]
-                        flag = True
-                    if count == 0 and flag:
-                        x1 = coord
-                    elif count == 1 and flag:
-                        y1 = coord
-                    elif count == 2 and flag:
-                        x2 = coord
-                    else:
-                        if flag:
+                # for the given origin, find the smallest norm from (x1,y1) of the rect
+                if math.sqrt( math.pow((origin[0] - update[0]),2) + math.pow((origin[1] - update[1]),2)  )  < smallest:
+                    # if a new smallest found, then update the coords
+                    count = 0
+                    for coord in update:
+                        co += str(coord)  
+                        if count == 0:
+                            x1 = coord
+                        elif count == 1:
+                            y1 = coord
+                        elif count == 2:
+                            x2 = coord
+                        elif count == 3:
                             y2 = coord
-                    count += 1
-                if flag:
+                        count += 1
                     x_marg = x2 - x1
                     y_marg = y2 - y1
-                sock.sendto( (co).encode(), (UDP_IP, UDP_PORT) )
+                # sock.sendto( (co).encode(), (UDP_IP, UDP_PORT) )
             # save the cropped image for unity to pull
             crop_img = img[y1:y1+y_marg, x1:x1+x_marg].copy()
-            cv.imwrite('savedImage.png', crop_img)
-            sock.sendto( 'updated'.encode(), (UDP_IP, UDP_PORT) )
+
+            cv.imwrite('fullimage.jpg', img)        # saves raw image 
+            cv.imwrite('savedImage.jpg', crop_img)  # saves face image for unity
+            sock.sendto( 'updated'.encode(), (UDP_IP, UDP_PORT) )   # sends the coords for face
 
         # exit out of program if pressing escape
         if cv.waitKey(5) == 27:
             break
-
     print('Done')
 
 
