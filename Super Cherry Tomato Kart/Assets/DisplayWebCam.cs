f07a3e7f81170866193f196b65a868a310c362fd
patrickchau <patrickchau59@gmail.com>
2020-02-25 13:35:43 -0800

    Integrated the face detection and display into main project Unity

---
+++ Super Cherry Tomato Kart/Assets/DisplayWebCam.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using UnityEngine.UI;
+using System.Text;
+using System.Threading;
+
+public class DisplayWebCam : MonoBehaviour
+{
+    string relpath;
+    // goes to super cherry tomato kartz
+    string addon = "FaceDetection\\savedImage.jpg";
+    string filepath;
+    string filepath2 = "C:\\Users\\Patrick Chau\\Documents\\GitHub\\EE180DB-Project\\Super Cherry Tomato Kart\\FaceDetection\\savedImage.jpg";
+    public bool new_image = false;
+
+    public void update_image(bool input)
+    {
+        new_image = input;
+    }
+
+    void pull_image()
+    {
+        byte[] fileData;
+        Texture2D texture = null;
+        print(filepath);
+        print("Does the file path exist?: " + System.IO.File.Exists(filepath) + "\n");
+        if (File.Exists(filepath))
+        {
+            print("pull image!");
+            fileData = File.ReadAllBytes(filepath);
+            texture = new Texture2D(2, 2);
+            texture.LoadImage(fileData);
+        }
+        Renderer rend = this.GetComponentInChildren<Renderer>();
+        rend.material.mainTexture = texture;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        //string filepath = Application.dataPath.Remove(Application.dataPath.Length-7, 7) + addon;
+        filepath = Path.GetFullPath(addon);
+        print(filepath);
+        print("Does the file path exist?: " + System.IO.File.Exists(filepath) + "\n");
+        print(filepath2);
+        print("Does the file path exist?: " + File.Exists(filepath2) + "\n");
+        pull_image();
+    }
+
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (new_image == true)
+        {
+            try
+            {
+                pull_image();
+            }
+            catch (Exception e)
+            {
+                print(e.ToString());
+            }
+        }
+    }
+}
---
+++ Super Cherry Tomato Kart/Assets/PlayerControllerScript.cs
@@ -0,0 +1,80 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+public class PlayerControllerScript : MonoBehaviour
+{
+
+    // 1. Declare Variables
+    Thread receiveThread; //1
+    UdpClient client; //2
+    int port; //3
+    GameObject go;
+    DisplayWebCam web;
+
+    // 2. Initialize variables
+    void Start()
+    {
+        port = 5065; //1 
+        InitUDP(); //4
+        client = new UdpClient(port); //1
+        go = GameObject.Find("Cube");
+        web = go.GetComponent<DisplayWebCam>();
+    }
+
+    // 3. InitUDP
+    private void InitUDP()
+    {
+        print("UDP Initialized");
+
+        receiveThread = new Thread(new ThreadStart(ReceiveData)); //1 
+        receiveThread.IsBackground = true; //2
+        receiveThread.Start(); //3
+
+    }
+
+    // 4. Receive Data
+    private void ReceiveData()
+    {
+
+
+        while (true) //2
+        {
+            string text = "";
+            try
+            {
+                IPEndPoint anyIP = new IPEndPoint(IPAddress.Parse("0.0.0.0"), port); //3
+                byte[] data = client.Receive(ref anyIP); //4
+
+                text = Encoding.UTF8.GetString(data); //5
+                print(">> " + text);
+
+            }
+            catch (Exception e)
+            {
+                // print out error type
+                print(e.ToString()); //7
+            }
+
+            if (!(string.IsNullOrEmpty(text)))
+            {
+                // if the string has some content
+                if (text == "updated")
+                {
+                    // then we want to pull the new texture
+                    web.update_image(true);
+                }
+            }
+        }
+    }
+
+    // 6. Check for variable value, and make the Player Jump!
+    void Update()
+    {
+
+    }
+}
