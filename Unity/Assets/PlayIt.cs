0ff223e5f6c9143e2deebd2a59a1c4e1507f0034
XILAI ZHANG <xilaizhang@g.ucla.edu>
2020-03-09 21:26:46 -0700

    repush the change

---
+++ Unity/Assets/PlayIt.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayIt : MonoBehaviour
+{
+    public void PlayScene(string sceneName){
+    	Debug.Log("load another scene");
+    	SceneManager.LoadScene(sceneName);
+    }
+}
